1dbd19469270892f57ae6813a90ee27243dd7e12
Suman Michael <sumanmichael01@gmail.com>
2021-08-30 11:17:54 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import glob
-import os
 
 from PIL import ImageOps
 
@@ -7,10 +6,10 @@ from GCVUtils import *
 
 set_config_file("gocr-creds.json")
 
-IMAGE_LOCATION = "data"
-DATASET_LOCATION = "data/dataset"
+IMAGE_IMPORT_LOCATION = "data"
+DATASET_EXPORT_LOCATION = "data/dataset"
 
-for img_path in glob.glob(os.path.join(IMAGE_LOCATION, "*.jpg")):
+for img_path in glob.glob(os.path.join(IMAGE_IMPORT_LOCATION, "*.jpg")):
     print("STARTING:", img_path)
     filename = img_path.split(os.sep)[-1].split(".")[0]
 
@@ -21,30 +20,30 @@ for img_path in glob.glob(os.path.join(IMAGE_LOCATION, "*.jpg")):
     image.save(img_path)
 
     response = get_api_response(img_path)
-    os.makedirs(os.path.join(DATASET_LOCATION, filename), exist_ok=True)
-    save_annotation_file(os.path.join(DATASET_LOCATION, filename, f"{filename}_gcv_response.json"), response)
+    os.makedirs(os.path.join(DATASET_EXPORT_LOCATION, filename), exist_ok=True)
+    save_annotation_file(os.path.join(DATASET_EXPORT_LOCATION, filename, f"{filename}_gcv_response.json"), response)
 
     concave_bounds = get_bounds_from_annotation_file(
-        os.path.join(DATASET_LOCATION, filename, f"{filename}_gcv_response.json"), is_concave=True)
+        os.path.join(DATASET_EXPORT_LOCATION, filename, f"{filename}_gcv_response.json"), is_concave=True)
 
     rect_bounds = get_bounds_from_annotation_file(
-        os.path.join(DATASET_LOCATION, filename, f"{filename}_gcv_response.json"), is_concave=False)
+        os.path.join(DATASET_EXPORT_LOCATION, filename, f"{filename}_gcv_response.json"), is_concave=False)
 
-    with open(os.path.join(DATASET_LOCATION, filename, f"{filename}_concave_hulls.json"), "w",
+    with open(os.path.join(DATASET_EXPORT_LOCATION, filename, f"{filename}_concave_hulls.json"), "w",
               encoding="utf-8") as f:
         json.dump(concave_bounds, f, ensure_ascii=False)
 
-    with open(os.path.join(DATASET_LOCATION, filename, f"{filename}_rect_bounds.json"), "w",
+    with open(os.path.join(DATASET_EXPORT_LOCATION, filename, f"{filename}_rect_bounds.json"), "w",
               encoding="utf-8") as f:
         json.dump(rect_bounds, f, ensure_ascii=False)
 
-    with open(os.path.join(DATASET_LOCATION, filename, f"{filename}.txt"), "w", encoding="utf-8") as f:
+    with open(os.path.join(DATASET_EXPORT_LOCATION, filename, f"{filename}.txt"), "w", encoding="utf-8") as f:
         for i, (coords, rect) in enumerate(zip(concave_bounds, rect_bounds)):
             get_cropped_masked_image(image, coords["vertices"], rect["vertices"]).save(
-                os.path.join(DATASET_LOCATION, filename, f'{filename}-l{i}.jpg'))
+                os.path.join(DATASET_EXPORT_LOCATION, filename, f'{filename}-l{i}.jpg'))
             f.write(f'{filename}-l{i}.jpg {rect["text"]}\n')
 
     image = draw_polygons(image, bounds=[bound["vertices"] for bound in concave_bounds])
     # image = draw_bounds_for_features(image, os.path.join(DATASET_LOCATION, filename, f"{filename}_gcv_response.json"),[FeatureType.WORD])
-    image.save(os.path.join(DATASET_LOCATION, f'{filename}_lines_annotated.jpg'))
+    image.save(os.path.join(DATASET_EXPORT_LOCATION, f'{filename}_lines_annotated.jpg'))
     # image.save(os.path.join(DATASET_LOCATION, f'{filename}_lines_annotated_rect.jpg'))
